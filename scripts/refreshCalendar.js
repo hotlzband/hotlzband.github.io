befbed79578b83d9cd4a16c066f6236946c8a179
werkheiser <bill.werkheiser@gmail.com>
2015-10-11 22:48:27 -0500

    Read existing dates file to determine if we need to update

--- scripts/refreshCalendar.js
+++ scripts/refreshCalendar.js
@@ -1,31 +1,51 @@
+//Take the current dates file and conpare it against the new file.
+//If anything changed, push the new file out there.
+
 var ical = require('ical');
 var jsonfile = require('jsonfile')
 
 var currentDate = new Date();
-var outFile = '../objects/dates.json'
+var dateFile = '../objects/dates.json'
+var overWrite = false;
+var lastMod = 0;
 
+//Grab latest mod date from current file
+var currentDates = jsonfile.readFileSync(dateFile);
+for (var k in currentDates) {
+	if (currentDates.hasOwnProperty(k)) {
+		var ev = currentDates[k]
+		if (ev['last-modified'].substring(0,7) > lastMod) {
+			lastMod = ev['last-modified'];
+		}
+	}
+}
+console.log('Last modified entry for current file:' + lastMod);
+
+//Grab ical from web & compare latest mod dates.  Overwrite if necessary
 ical.fromURL('https://www.google.com/calendar/ical/n9if4js3l7d3k8u57un90s7ueg%40group.calendar.google.com/public/basic.ics', {}, function(err, data) {
-	var lastMod = 0;
 	var outString = [];
-	
+
 	//console.log(data);
-	//for (var k = data.length -1; k >= 0 ; k--){
 	for (var k in data){
 		if (data.hasOwnProperty(k)) {
 			var ev = data[k]
-			if (ev['last-modified'] > lastMod) {
-				lastMod = ev['last-modified'];
+			if (ev['last-modified'].substring(0,7) > lastMod) {
+				overWrite = true;
+				console.log('Consenting overwrite. Entry ' + ev['uid'] + ' modified at:' + ev['last-modified']);
 			}
+
+			//Only grab future dates
 			var start = new Date(ev['start']);
 			if (start >= currentDate ) {
-				console.log('Adding uid: ' + ev['uid']);
+				console.log('Adding event with uid: ' + ev['uid']);
 				outString.push(ev);
 			}
 		}
 	}
-	
-	jsonfile.writeFile(outFile, outString, function (err) {
-		console.error(err)
-	});
-});
 
+	if(overWrite){
+		jsonfile.writeFile(dateFile, outString, function (err) {
+			console.error(err)
+		});
+	}
+});
